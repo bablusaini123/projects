51085c514a07de84957d44dd2a3d1426d56c7781
bablusaini123 <bablusaini90310@gmail.com>
2024-06-01 22:57:54 +0530

    disabled

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: '91club'
+    host: 'bktbnwcreyneunro51tz-mysql.services.clever-cloud.com',
+    user: 'uybeyzpoagkwjqz6',
+    password: 'gj6fDGgaUdwxsRPeAlDG',
+    database: 'bktbnwcreyneunro51tz'
 });
 
 export default pool;
