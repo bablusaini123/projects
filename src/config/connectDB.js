e2a02f292aadd3e2b51b622687f08fe989ba650f
bablusaini123 <bablusaini90310@gmail.com>
2024-06-11 18:21:51 +0530

    change db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'bktbnwcreyneunro51tz-mysql.services.clever-cloud.com',
     user: 'uybeyzpoagkwjqz6',
-    password: 'uybeyzpoagkwjqz6',
+    password: 'i09EKR8NDSEVFDhvnAl1',
     database: 'bktbnwcreyneunro51tz'
 });
 
